5bec1096c5deaa5d0db92da5bbd267d603c42888
timucin <135065802+Proj-CX@users.noreply.github.com>
2024-02-24 15:35:57 +0300

    Update make.sh

--- Linux-USM-SetupFile-tr/make.sh
+++ Linux-USM-SetupFile-tr/make.sh
@@ -25,15 +25,19 @@ mv USM-ayarlar-programcigi.sh Ayarlar.sh
 if command -v apt &> /dev/null; then
   sudo apt install zenity
   sudo apt install xterm
+  sudo apt install vim
 elif command -v yum &> /dev/null; then
   sudo yum install zenity
   sudo yum install xterm
+  sudo yum install vim
 elif command -v dnf &> /dev/null; then
   sudo dnf install zenity
-  sudo dnf install xterm 
+  sudo dnf install xterm
+  sudo dnf install vim
 elif command -v pacman &> /dev/null; then
   sudo pacman -S zenity
   sudo pacman -S xterm
+  sudo pacman -S vim
 else
   echo "USM-Ayarlar ; Paket yükleyicisi bulunamadığı için otamatik Zenity ve xterm paketi kurulamadı Zenity ve xterm paketini kurmanız gerekir."
 fi
